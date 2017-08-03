a0d7045f3981894bc9d694876e19a4b83206bdab
CharlesSchimmel <charlesschimmel@gmail.com>
2017-08-03 16:55:51 -0500

    updated stuff

--- armitage-bot/armitage-bot.py
+++ armitage-bot/armitage-bot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-__name__="armitage-bot"
 __version__="0.1"
 
 import praw
@@ -18,25 +17,24 @@ from fuzzywuzzy import process
 def pull_arkham_cards(cardrange):
 	arkhamdb_api = "http://arkhamdb.com/api/public/card/{}"
 	for cardnum in cardrange:
-		with sqlite3.connect('arkham.db') as con:
-			missing = con.execute("select * from cards where ID = ?",(cardnum,)).fetchone() != None
-			if not missing:
+		with sqlite3.connect('armitage.db') as con:
+			missing = con.execute("select * from arkham_cards where ID = ?",(cardnum,)).fetchone() == None
+			if missing:
 				padded_cardnum = "0"+str(cardnum)
 				req = requests.get(arkhamdb_api.format(padded_cardnum))
 				if req.status_code == 200:
 					card_code=req.json()['code']
 					card_name=req.json()['name']
 					card_url=req.json()['url']
-					print("inserting {} {} {}".format(card_code,card_name,card_url))
-					insert_query = "insert into cards (ID,name,url) values (?,?,?)"
+					insert_query = "insert into arkham_cards (ID,name,url) values (?,?,?)"
 					cur = con.execute(insert_query,(card_code,card_name,card_url))
 					con.commit()
 					time.sleep(0.25)
 		# cardnum += 1
 
 def build_arkham_dict():
-	with sqlite3.connect('arkham.db') as con:
-		cur = con.execute("select name,url from cards")
+	with sqlite3.connect('armitage.db') as con:
+		cur = con.execute("select name,url from arkham_cards")
 		all_cards = cur.fetchall()
 	arkham_dict = {}
 	for card in all_cards:
@@ -44,12 +42,23 @@ def build_arkham_dict():
 	return arkham_dict
 
 def build_netrunner_dict():
-	with sqlite3.connect('arkham.db') as con:
-		cur = con.execute("select name,url from cards")
+	with sqlite3.connect('armitage.db') as con:
+		cur = con.execute("select name,url from netrunner_cards")
 		all_cards = cur.fetchall()
 	arkham_dict = {}
 	for card in all_cards:
 		arkham_dict[card[0]] = card[1]
+
+def log_comment(comment):
+	with sqlite3.connect('armitage.db') as con:
+		con.execute("insert into comments (Id) values (?)",(str(comment),))
+		con.commit()
+
+def already_replied(comment):
+		with sqlite3.connect('armitage.db') as con:
+			query_string = "select * from comments where Id = ?"
+			cur = con.execute(query_string,(str(comment),))
+			return cur.fetchone() != None
 
 def fuzzy_match_card(card_dict,card_name):
 	ratio_max = 0
@@ -63,28 +72,58 @@ def fuzzy_match_card(card_dict,card_name):
 	return max_card
 
 def get_ids_by_name(card_name):
-	with sqlite3.connect('arkham.db') as con:
-		query_string = ("select ID from cards where name = (?)")
+	with sqlite3.connect('armitage.db') as con:
+		query_string = ("select ID from arkham_cards where name = (?)")
 		cur = con.execute(query_string,(card_name,))
 		results = cur.fetchall()
 	return [i[0] for i in results]
 
-def get_card_details(card_id):
+def get_arkham_card_details(card_id):
 	arkhamdb_api = "http://arkhamdb.com/api/public/card/{}"
-	possible_properties = ["name","type_name","cost","xp","faction_name","slot","traits","text"]
+	possible_properties = ["name","type_name","cost","xp","faction_name","slot","traits","text","url"]
 	padded_id = "0"+str(card_id)
 	req = requests.get(arkhamdb_api.format(padded_id))
+	reply = ""
 	for p in possible_properties:
 		if p in req.json().keys():
-			print("{}: {}".format(p,req.json()[p]))
+			reply += "{}: {}".format(p,replace_html(req.json()[p]))
+			reply += "\n"
+	return reply
 
-def sieve_cards_from_comment(comment):
-    # return re.findall(r"""\?([A-z']+\s*[A-z]\w+\s*)*\?""",comment)
-    return re.findall(r"""\?([A-z'! \"]+)*\?""",comment)
+def replace_html(text):
+	text = str(text)
+	return re.sub(r"""(<\/*b>)""","*",text)
 
-comment = "what about cards with spaces like ?\"i've got a plan\"?"
-print(sieve_cards_from_comment(comment))
-for fuzzy_name in sieve_cards_from_comment(comment):
-	card_name = fuzzy_match_card(build_arkham_dict(),fuzzy_name)
-	for cid in get_ids_by_name(card_name):
-		get_card_details(cid)
+def sieve_cards_from_comment(comment_body):
+	return re.findall(r"""\?([A-z0-9'.! \"]+)*\?""",comment_body)
+
+def watch_comments():
+	sub = "bottest"
+	for comment in r.subreddit(sub).stream.comments():
+		reply = ""
+		if comment.created_utc > (time.time() - 120) and not already_replied(comment):
+			possible_cards = sieve_cards_from_comment(comment.body)
+			if possible_cards:
+				for fuzzy_name in possible_cards:
+					card_name = fuzzy_match_card(arkham_dict,fuzzy_name)
+					for cid in get_ids_by_name(card_name):
+						reply += get_arkham_card_details(cid)
+				reply_to_comment(comment,reply)
+
+def reply_to_comment(comment,reply):
+	log_comment(comment)
+	try:
+		comment.reply(reply)
+		print(reply)
+	except Exception as e:
+		print("reply error:")
+		print(e)
+
+r = praw.Reddit(site_name="armitage",)
+arkham_dict = build_arkham_dict()
+
+if __name__ == "__main__":
+	try:
+		watch_comments()
+	except Exception as e:
+		print(e)
